59bb4bce620911c5fc1611582e25cde777fffd2c
Tymothé BILLEREY <tymothe.billerey@gmail.com>
2023-11-27 10:13:44 +0000

    Update null-ls.lua

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -11,6 +11,7 @@ local opts = {
     null_ls.builtins.formatting.asmfmt,
     null_ls.builtins.formatting.latexindent,
     null_ls.builtins.code_actions.proselint,
+    null_ls.builtins.formatting.rome,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
