4c6f780779ae7d70396f0c71bb3c834d369ae87b
neptumiun <tymothe.billerey@gmail.com>
2024-04-01 15:32:52 +0200

    refactor for stow gestion

--- config/scripts/battry.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-battery_percentage=$(cat /sys/class/power_supply/BAT*/capacity | awk '{s+=$1} END {print int(s/NR)}')
-
-if [ "$battery_percentage" -le 90 ]; then
-    dunstify --urgency=critical "Battery Low" "Battery level is ${battery_percentage}%. Please connect the charger!"
-fi
--- install.sh
+++ install.sh
@@ -1,16 +1,3 @@
 #/bin/bash
 
-PWD=$(pwd)
-mkdir -p $HOME/.config
-ln -s $PWD/nvim $HOME/.config/nvim
-ln -s $PWD/config/i3 $HOME/.config/i3
-ln -s $PWD/config/polybar $HOME/.config/polybar
-ln -s $PWD/config/scripts $HOME/.config/scripts
-ln -s $PWD/config/systemd $HOME/.config/systemd
-ln -s $PWD/config/zathura $HOME/.config/zathura
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/Xresources $HOME/.Xresources
-ln -s $PWD/gitconfig $HOME/.gitconfig
-ln -s $PWD/gitattributes $HOME/.gitattributes
-ln -s $PWD/config/dunst $HOME/.config/dunst
-ln -s $PWD/config/tmux/ $HOME/.config/tmux
+stow -t $HOME .
