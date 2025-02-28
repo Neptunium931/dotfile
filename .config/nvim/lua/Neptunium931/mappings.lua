61d216e755c36e0205415badcdfc9a3ae87049bf
neptumiun <tymothe.billerey@gmail.com>
2025-02-28 16:36:43 +0100

    refactor(nvim): mappings

--- .config/nvim/lua/Neptunium931/mappings.lua
+++ .config/nvim/lua/Neptunium931/mappings.lua
@@ -1,34 +1,21 @@
 local M = {}
 
-M.general = {
-  n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-  }
-}
-
-M.general = {
-  n = {
-    -- close buffer
-    ["<S-q>"] = { "<cmd>Bdelete<CR>", "close buffer"},
-  }
-}
-
-M.vimtex = {
-  plugin = true,
-  n = {
-    ["<leader>lv"] = {
-      function()
-        vim.cmd("VimtexView")
-      end
-    },
-    ["<leader>ll"] = {
-      function()
-        vim.cmd("VimtexCompile")
-      end
-    }
+M.n = {
+  ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+  ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+  ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+  ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  -- close buffer
+  ["<S-q>"] = { "<cmd>Bdelete<CR>", "close buffer"},
+  ["<leader>lv"] = {
+    function()
+      vim.cmd("VimtexView")
+    end
+  },
+  ["<leader>ll"] = {
+    function()
+      vim.cmd("VimtexCompile")
+    end
   }
 }
 return M
