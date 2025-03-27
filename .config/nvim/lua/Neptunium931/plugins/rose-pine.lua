ebd5ead280762c7d55c0d8a7c765462922404cef
neptumiun <tymothe.billerey@gmail.com>
2025-03-27 09:48:21 +0100

    feat(nvim): add colorscheme hour changes

--- .config/nvim/lua/Neptunium931/plugins/rose-pine.lua
+++ .config/nvim/lua/Neptunium931/plugins/rose-pine.lua
@@ -1,21 +1,28 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+vim.api.nvim_create_user_command('ColorMyPencils',
+function ()
+  local hour = tonumber(os.date("%H"))
+  local color = "rose-pine-dawn"
+  if hour < 19 and hour > 7 then
+    color = "rose-pine-dawn"
+  else
+    color = "rose-pine-moon"
+  end
+  vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
+  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end , {})
 
-return { 
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require('rose-pine').setup({
-			styles = {
-				italic = false,
-			},
-		})
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require('rose-pine').setup({
+      styles = {
+        italic = false,
+      },
+    })
 
-		ColorMyPencils();
-	end
+    vim.cmd.ColorMyPencils();
+  end
 }
